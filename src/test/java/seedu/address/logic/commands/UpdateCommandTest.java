4bb58913644e11a277a119626b5831d5194cec40
DESKTOP-1NRGIML\user <gypreal@gmail.com>
2018-09-25 00:05:27 +0800

    Add tests for the new UpdateCommand feature

---
+++ src/test/java/seedu/address/logic/commands/UpdateCommandTest.java
@@ -0,0 +1,48 @@
+package seedu.address.logic.commands;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MEDICAL_RECORD_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MEDICAL_RECORD_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.UpdateCommand.MESSAGE_ARGUMENTS;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class UpdateCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute() throws Exception {
+        final String medicalRecord = "Some medical record";
+        assertCommandFailure(new UpdateCommand(INDEX_FIRST_PERSON, medicalRecord), model, commandHistory,
+                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), medicalRecord));
+    }
+
+    @Test
+    public void equals() {
+        final UpdateCommand standardCommand = new UpdateCommand(INDEX_FIRST_PERSON, VALID_MEDICAL_RECORD_AMY);
+        // same values -> returns true
+        UpdateCommand commandWithSameValues = new UpdateCommand(INDEX_FIRST_PERSON, VALID_MEDICAL_RECORD_AMY);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new UpdateCommand(INDEX_SECOND_PERSON, VALID_MEDICAL_RECORD_AMY)));
+        // different remarks -> returns false
+        assertFalse(standardCommand.equals(new UpdateCommand(INDEX_FIRST_PERSON, VALID_MEDICAL_RECORD_BOB)));
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/parser/UpdateCommandParserTest.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICAL_RECORD;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.UpdateCommand;
+
+public class UpdateCommandParserTest {
+
+    private UpdateCommandParser parser = new UpdateCommandParser();
+
+    @Test
+    public void parse_indexSpecified_failure() throws Exception {
+        final String medicalRecord = "Some medical record.";
+        // have remarks
+        Index targetIndex = INDEX_FIRST_PERSON;
+        String userInput = targetIndex.getOneBased() + " " + PREFIX_MEDICAL_RECORD.toString() + " " + medicalRecord;
+        UpdateCommand expectedCommand = new UpdateCommand(INDEX_FIRST_PERSON, medicalRecord);
+        assertParseSuccess(parser, userInput, expectedCommand);
+        // no remarks
+        userInput = targetIndex.getOneBased() + " " + PREFIX_MEDICAL_RECORD.toString();
+        expectedCommand = new UpdateCommand(INDEX_FIRST_PERSON, "");
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+    @Test
+    public void parse_noFieldSpecified_failure() throws Exception {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, UpdateCommand.MESSAGE_USAGE);
+        // nothing at all
+        assertParseFailure(parser, UpdateCommand.COMMAND_WORD, expectedMessage);
+    }
+} 
