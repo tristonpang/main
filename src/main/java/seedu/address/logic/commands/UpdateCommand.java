dd1c54ff4b5acbb929b99bcf361a9e0a9f13a973
DESKTOP-1NRGIML\user <gypreal@gmail.com>
2018-09-25 00:04:38 +0800

    Add UpdateCommand which accepts 2 inputs index and string

---
+++ src/main/java/seedu/address/logic/commands/UpdateCommand.java
@@ -0,0 +1,67 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICAL_RECORD;
+
+public class UpdateCommand extends Command {
+
+    public static final String COMMAND_WORD = "update";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": updates the medical record of the person identified by the index number used in the last person listing."
+            + "Existing medical record will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + PREFIX_MEDICAL_RECORD + "REMARK\n"
+            + "Example: " + COMMAND_WORD + " 1"
+            + PREFIX_MEDICAL_RECORD + "Diagnosed with flue. Tamiflu prescribed.";
+
+    public static final String MESSAGE_UPDATE_MEDICAL_RECORD_SUCCESS = "Medical record updated";
+
+    public static final String MESSAGE_UPDATE_MEDICAL_RECORD_FAILURE = "Medical record does nothing";
+
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    private final Index index;
+    private final String medicalRecord;
+
+    public UpdateCommand(Index index, String medicalRecord) {
+        requireNonNull(index);
+        requireNonNull(medicalRecord);
+        this.index = index;
+        this.medicalRecord = medicalRecord;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), medicalRecord));
+        /**
+         try {
+         return null;
+         } catch (Exception e) {
+         throw e;
+         }
+         **/
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof UpdateCommand)) {
+            return false;
+        }
+        // state check
+        UpdateCommand e = (UpdateCommand) other;
+        return index.equals(e.index)
+                && medicalRecord.equals(e.medicalRecord);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICAL_RECORD;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.UpdateCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+public class UpdateCommandParser implements Parser<UpdateCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemarkCommand
+     * and returns an RemarkCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UpdateCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_MEDICAL_RECORD);
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UpdateCommand.MESSAGE_USAGE));
+        }
+        String remark = argMultimap.getValue(PREFIX_MEDICAL_RECORD).orElse("");
+        return new UpdateCommand(index, remark);
+    }
+}
