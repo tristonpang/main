7da556815f48d52d35cad67ef589bf2a35fe4831
DESKTOP-DAUN7QD\Kenneth <kennethgoh96.989@gmail.com>
2018-09-25 23:17:27 +0800

    List takes in optional parameter to list only patient/doctor

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -67,7 +67,7 @@ public class AddressBookParser {
             return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
-            return new ListCommand();
+            return new ListCommandParser().parse(arguments);
 
         case HistoryCommand.COMMAND_WORD:
             return new HistoryCommand();
