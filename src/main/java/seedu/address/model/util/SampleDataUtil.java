b1c356a238f0fad13e5ab3dd9b9024ec144093bc
DESKTOP-1NRGIML\user <gypreal@gmail.com>
2018-10-01 21:15:23 +0800

    Fix incorrect input

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,7 +24,7 @@ import seedu.address.model.tag.Tag;
  */
 public class SampleDataUtil {
 
-    public static final Appointment SAMPLE_APPOINTMENT = new Appointment("22.11.2018, 1300, 1400, Alice, Heart, Betty"
+    public static final Appointment SAMPLE_APPOINTMENT = new Appointment("22.11.2018,1300,1400,Alice,Heart,Betty"
             + ",S1234567A");
 
     public static Person[] getSamplePersons() {
