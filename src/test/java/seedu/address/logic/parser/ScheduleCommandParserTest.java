13c1334dbfa202a29a4a5d59111fc0c95039ff23
nelvintan <nelvintan@gmail.com>
2018-10-08 10:05:10 +0800

    update junit test to reflect new format of schedule command.

--- src/test/java/seedu/address/logic/parser/ScheduleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ScheduleCommandParserTest.java
@@ -1,7 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SCHEDULE;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -14,19 +15,29 @@ import seedu.address.model.person.Appointment;
 
 public class ScheduleCommandParserTest {
     private ScheduleCommandParser parser = new ScheduleCommandParser();
-    private final String nonEmptyAppointment = "22.11.2018,1300,1400,Alice,Heart,Betty,S1234567A";
+    private final String nonEmptyAppointment = PREFIX_DATE + "22.11.2018 "
+            + PREFIX_START_TIME + "1300 "
+            + PREFIX_END_TIME + "1400 "
+            + PREFIX_DOCTOR_NAME + "Alice "
+            + PREFIX_MEDICAL_DEPARTMENT + "Heart "
+            + PREFIX_PATIENT_NAME + "Betty "
+            + PREFIX_NRIC + "S1234567A ";
 
     @Test
     public void parse_indexSpecified_success() {
         // have remark
         Index targetIndex = INDEX_FIRST_PERSON;
-        String userInput = targetIndex.getOneBased() + " " + PREFIX_SCHEDULE + nonEmptyAppointment;
-        ScheduleCommand expectedCommand = new ScheduleCommand(INDEX_FIRST_PERSON, new Appointment(nonEmptyAppointment));
+        String userInput = targetIndex.getOneBased() + " " + nonEmptyAppointment;
+        ScheduleCommand expectedCommand = new ScheduleCommand(INDEX_FIRST_PERSON,
+                new Appointment("22.11.2018", "1300", "1400",
+                        "Alice", "Heart", "Betty", "S1234567A"));
         assertParseSuccess(parser, userInput, expectedCommand);
         // no remark
 
-        userInput = targetIndex.getOneBased() + " " + PREFIX_SCHEDULE;
-        expectedCommand = new ScheduleCommand(INDEX_FIRST_PERSON, new Appointment(""));
+        userInput = targetIndex.getOneBased() + " " + nonEmptyAppointment;
+        expectedCommand = new ScheduleCommand(INDEX_FIRST_PERSON,
+                new Appointment("22.11.2018", "1300", "1400",
+                "Alice", "Heart", "Betty", "S1234567A"));
         assertParseSuccess(parser, userInput, expectedCommand);
     }
 
