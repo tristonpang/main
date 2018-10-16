52de84e31abbfd728dd89b69858a4a52a2bea161
DESKTOP-1NRGIML\user <gypreal@gmail.com>
2018-10-16 13:20:47 +0800

    resolve conflict by using local files

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -31,7 +31,6 @@ public class SampleDataUtil {
 
     public static Person[] getSamplePersons() {
         return new Person[] {
-<<<<<<< HEAD
             new Patient(new Name("Alex Yeoh"), new Nric("S1234567A"), new Phone("87438807"),
                     new Email("alexyeoh@example.com"), new Address("Blk 30 Geylang Street 29, #06-40"),
                     getTagSet("friends"), SAMPLE_APPOINTMENT_LIST, new MedicalRecord("")),
@@ -53,28 +52,6 @@ public class SampleDataUtil {
             new Doctor(new Name("Roy Balakrishnan"), new Nric("S4040404D"), new Phone("92624417"),
                     new Email("royb@example.com"), new Address("Blk 45 Aljunied Street 85, #11-31"),
                     getTagSet("colleagues"), SAMPLE_APPOINTMENT_LIST,
-=======
-            new Patient(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), getTagSet("friends"), SAMPLE_APPOINTMENT_LIST,
-                    new Nric("S1234567A"), new MedicalRecord("")),
-            new Doctor(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getTagSet("colleagues", "friends"),
-                    SAMPLE_APPOINTMENT_LIST, new MedicalDepartment("Neurology")),
-            new Patient(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), getTagSet("neighbours"),
-                    SAMPLE_APPOINTMENT_LIST, new Nric("S2222222B"), new MedicalRecord("22.11.2018",
-                    "cough","Dextromethorphan", "")),
-            new Patient(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"), new Address(
-                    "Blk 436 Serangoon Gardens Street 26, #16-43"), getTagSet("family"),
-                    SAMPLE_APPOINTMENT_LIST, new Nric("S9991114A"), new MedicalRecord("10.10.2017",
-                            "Stage 3 testicular cancer", "Orchiectomy for both testicles.",
-                            "chances of survival 15%")),
-            new Doctor(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), getTagSet("classmates"), SAMPLE_APPOINTMENT_LIST,
-                new MedicalDepartment("Obstetrics")),
-            new Doctor(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), getTagSet("colleagues"), SAMPLE_APPOINTMENT_LIST,
->>>>>>> aecc44dfe7594f12eb967b93c90d344642541cae
                     new MedicalDepartment("Cardiology"))
         };
     }
