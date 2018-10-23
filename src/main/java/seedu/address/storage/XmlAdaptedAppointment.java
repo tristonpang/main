7f9f68af771e6abb6ab5b06392140e0336a6717b
nelvintan <nelvintan@gmail.com>
2018-10-23 12:06:56 +0800

    fix styling.

--- src/main/java/seedu/address/storage/XmlAdaptedAppointment.java
+++ src/main/java/seedu/address/storage/XmlAdaptedAppointment.java
@@ -1,7 +1,7 @@
 package seedu.address.storage;
 
-import javax.xml.bind.annotation.XmlValue;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Appointment;
