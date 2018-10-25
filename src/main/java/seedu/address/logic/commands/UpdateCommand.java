ef0a376ecf4dd05ea42235ea1ddb235613608125
GaryGohYP <gypreal@gmail.com>
2018-10-25 18:56:11 +0800

    Fix update command to now update the display panel after consecutive update commands

--- src/main/java/seedu/address/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/address/logic/commands/UpdateCommand.java
@@ -92,6 +92,10 @@ public class UpdateCommand extends Command {
         } catch (PersonNotFoundException pnfe) {
             throw new AssertionError("The target person cannot be missing");
         }
+        //Undoing and Redoing unselects a previously selected person.
+        //Doing so updates the display panel after an update. May consider abstracting this into an UnselectCommand.
+        new UndoCommand().execute(model, history);
+        new RedoCommand().execute(model, history);
         SelectCommand selectUpdatedPatient = new SelectCommand(index);
         selectUpdatedPatient.execute(model, history);
         return new CommandResult(generateSuccessMessage(editedPerson));
