905c82b88b4834f4168dde49a0516a849a43fd85
nelvintan <nelvintan@gmail.com>
2018-10-28 11:25:19 +0800

    fix styling errors.

--- src/test/java/seedu/address/model/person/UniquePersonListTest.java
+++ src/test/java/seedu/address/model/person/UniquePersonListTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.INVALID_NRIC_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.TypicalPersons.ALICE;
