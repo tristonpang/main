71cf9d9988c80d977e46f7d220b932bdb9fb2022
DESKTOP-DAUN7QD <kennethgoh96.989@gmail.com>
2018-11-08 23:29:55 +0800

    Update for new tests in NricTest

--- src/test/java/seedu/address/model/person/NricTest.java
+++ src/test/java/seedu/address/model/person/NricTest.java
@@ -26,27 +26,40 @@ public class NricTest {
         Assert.assertThrows(NullPointerException.class, () -> Nric.isValidNric(null));
 
         // invalid nric
-        assertFalse(Nric.isValidNric("")); // empty string
-        assertFalse(Nric.isValidNric(" ")); // spaces only
-        assertFalse(Nric.isValidNric("^")); // only non-alphanumeric characters
-        assertFalse(Nric.isValidNric("s1234567*")); // contains non-alphanumeric characters
-        assertFalse(Nric.isValidNric("s123*567B")); // contains non-alphanumeric characters
-        assertFalse(Nric.isValidNric("s12345670")); // does not end with a character
-        assertFalse(Nric.isValidNric("P1234567a")); // does not start with a valid character
-        assertFalse(Nric.isValidNric("11234567A")); // does not start with a character
-        assertFalse(Nric.isValidNric("s12345673333333J")); // more than 7 digits
-        assertFalse(Nric.isValidNric("s12J")); // less than 7 digits
-        assertFalse(Nric.isValidNric("ssss1234567j")); // more than 2 starting characters
-        assertFalse(Nric.isValidNric("s1234567ttttttt")); // more than 2 ending characters
-        assertFalse(Nric.isValidNric("s1234   567t")); // whitespaces in between nric code
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric(""))); // empty string
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric(" "))); // spaces only
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("^"))); // only non-alphanumericcharacters
+        Assert.assertThrows(IllegalArgumentException.class, () ->
+                assertFalse(Nric.isValidNric("F2435126*"))); // contains non-alphanumeric characters
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("F243*126L"))); // contains non-alphanumeric characters
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("F2435126"))); // does not end with a character
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("P2435126L"))); // does not start with a valid character
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("2435126L"))); // does not start with a character
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("s12345673333333J"))); // more than 7 digits
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("s12J"))); // less than 7 digits
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("FFFFF2435126L"))); // more than 2 starting characters
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("F2435126LLLLLL"))); // more than 2 ending characters
+        Assert.assertThrows(IllegalArgumentException.class,
+                () -> assertFalse(Nric.isValidNric("F243    5126L"))); // whitespaces in between nric code
 
         // valid nric
-        assertTrue(Nric.isValidNric("S1234567A")); // all caps
-        assertTrue(Nric.isValidNric("s2234567a")); // no caps
-        assertTrue(Nric.isValidNric("s1235567A")); // last character in caps
-        assertTrue(Nric.isValidNric("S9234567a")); // first character in caps
-        assertTrue(Nric.isValidNric("T1234567A")); // start with F
-        assertTrue(Nric.isValidNric("F1234567A")); // start with T
-        assertTrue(Nric.isValidNric("G1234567A")); // start with G
+        assertTrue(Nric.isValidNric("S2200025Z")); // all caps
+        assertTrue(Nric.isValidNric("s2200025z")); // no caps
+        assertTrue(Nric.isValidNric("s2200025Z")); // last character in caps
+        assertTrue(Nric.isValidNric("S2200025z")); // first character in caps
+        assertTrue(Nric.isValidNric("F2435126L")); // start with F
+        assertTrue(Nric.isValidNric("T3569534B")); // start with T
+        assertTrue(Nric.isValidNric("G6189344Q")); // start with G
     }
 }
